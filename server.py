9659802c44e617385c16568e7e283c2ece2633d0
samyak-jain <samtan106@gmail.com>
2019-03-16 02:27:02 +0530

    Finished basic auth

--- server.py
+++ server.py
@@ -5,18 +5,23 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.escape
-import jwt
+import pathlib
+import firebase_admin
 
-from functools import wraps
-from typing import Callable, Optional, Any
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin.db import Reference
+from functools import wraps, lru_cache
+from typing import Callable, Optional, Any, Dict, List
 from dotenv import load_dotenv
 from tornado.escape import native_str, parse_qs_bytes
-from motor import motor_tornado as MT
 from tornado.options import define, options
+from pathlib import Path
 
 
 define("port", default=8080, help="runs on the given port", type=int)
 
+"""
 def protected(f: Callable[..., None]) -> Optional[Callable[..., None]]:
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -40,15 +45,22 @@ def Authenticated(f: Callable[..., None]) -> Callable[..., None]:
         else:
             raise MyAppException(reason='User is not logged in.', status_code=301)
     return wrapper
-
+"""
 
 class MyAppException(tornado.web.HTTPError):
     pass
 
 class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self) -> MT.MotorDatabase:
-        return self.settings['db_client']
+    @staticmethod
+    def db() -> Reference:
+        cred = credentials.Certificate(Path(f"./{os.environ['SERVICE_ACCOUNT_FILE']}"))
+
+        # Initialize the app with a service account, granting admin privileges
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': os.environ['DB_URL']
+        })
+
+        return db.reference('agora_broadcasting')     
 
     def prepare(self) -> None:
         self.set_header('Content-Type', 'application/json')
@@ -98,21 +110,33 @@ class PingHandler(BaseHandler):
 class AuthHandler(BaseHandler):
     async def post(self) -> None:
         data = parse_qs_bytes(native_str(self.request.body), keep_blank_values=True)
-        extract_data(data, ["name", "uid", "broadcaster"])
-        
+        self.extract_data(data, ["name", "uid", "broadcaster"])
+
+        db = self.db()
+        users = db.child("users") # Get collection
+
+        users.child(self.uid).set({
+                "name": self.name,
+                "broadcaster": self.broadcaster
+            })
+
+        self.write(json.dumps({
+                'status_code': 200,
+                'message': f'Added {self.name} to Database'
+            }))
+
+
 
 
 if __name__ == "__main__":
     load_dotenv()
     options.parse_command_line()
-    client = getattr(MT.MotorClient(os.environ['MONGO_URI']), os.environ['DB_NAME'])
     app = tornado.web.Application(
         handlers=[
             (r"/", PingHandler),
             (r"/auth", AuthHandler)
         ],
         default_handler_class = my404handler,
-        db_client = client,
     )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(int(os.environ.get('PORT', options.port)))
